2f19a0793512a766391b93661c8a3642e2bf4e08
Yusuf Putra <yusufputra99@gmail.com>
2023-07-26 21:07:52 +0700

    feat(android): init splash screen

--- android/app/src/main/java/com/bangjamincodingtest/MainActivity.java
+++ android/app/src/main/java/com/bangjamincodingtest/MainActivity.java
@@ -1,5 +1,8 @@
 package com.bangjamincodingtest;
 
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -14,6 +17,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "bangJaminCodingTest";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
   }
 
   /**
